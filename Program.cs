18f8993a3f4383707d00fa26e43b3bb16103b90b
Manuel Seet <e0838385@u.nus.edu>
2021-10-23 18:33:24 +0800

    Basic program

--- Program.cs
+++ Program.cs
@@ -10,6 +10,31 @@ namespace OOPCS_Revision
     {
         static void Main(string[] args)
         {
+            SavingsAccount sa1 = new SavingsAccount("Diego");
+
+            SavingsAccount.baseInterestRate = 1.00f;
+
+            SavingsAccount sa2 = new SavingsAccount("Manny");
+
+            sa1.printIR();
+            sa2.printIR();
+        }
+
+        public class SavingsAccount
+        {
+            public static double baseInterestRate = 0.05;
+
+            public string Name { get; set; }
+
+            public SavingsAccount(string Name)
+            {
+                this.Name = Name;
+            }
+            
+            public void printIR()
+            {
+                Console.WriteLine($"{baseInterestRate}");
+            }
         }
     }
 }
